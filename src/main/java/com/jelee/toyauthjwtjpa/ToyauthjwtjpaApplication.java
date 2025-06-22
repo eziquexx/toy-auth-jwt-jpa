62488a3e6155410d817e5d486413013e85a00c5d
eziquexx <waftyann@gmail.com>
2025-06-23 01:02:43 +0900

    25.06.23 프로젝트 생성 및 초기화

---
+++ src/main/java/com/jelee/toyauthjwtjpa/ToyauthjwtjpaApplication.java
@@ -0,0 +1,13 @@
+package com.jelee.toyauthjwtjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyauthjwtjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyauthjwtjpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jelee/toyauthjwtjpa/ToyauthjwtjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jelee.toyauthjwtjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyauthjwtjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
